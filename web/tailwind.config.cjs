92aa6ed2329c2b42044b4775c563d0b81bb86f76
Adam <adam.brickhill@lonetree.xyz>
2023-05-10 21:59:48 +0800

    slide show update

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'slide-green': '#00fdc8',
+				'slide-purple': '#7c7ce0'
+			}
+		}
 	},
 	plugins: [],
 }
